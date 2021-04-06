350e3825328900e8e34077051d12197c181ce832
PhilippeMoesch <philippmoesch@gmail.com>
2021-04-06 22:42:02 +0200

    update runit

--- runit.sh
+++ runit.sh
@@ -8,7 +8,7 @@
 
 echo ibench-cpu ibench-l1d ibench-l1i ibench-l2 ibench-llc ibench-membw none
 
-read inter
+read inter 
 
 if [[ "$inter" != "none" ]]; then
     kubectl create -f interference/"$inter".yaml
@@ -36,9 +36,12 @@ count=1; # number of runs
 
 if [[ "$benchmark" == "fft" || "$benchmark" == "all" ]]; then
     for i in $(seq $count); do
+        echo running fft
         kubectl create -f parsec-benchmarks/part2a/parsec-fft.yaml
-        echo run number $i, press key to go on
-        read ok # sleep ...
+        echo run number $i #, press key to go on
+        echo sleeping...
+        sleep 3m # 2 minutes was not enough sometimes
+        #read ok # sleep ...
         ans=$(kubectl logs $(kubectl get pods --selector=job-name=parsec-splash2x-fft --output=jsonpath='{.items[*].metadata.name}'))
         echo "${ans}" | grep user
         #echo "${ans}"
@@ -48,9 +51,12 @@ fi
 
 if [[ "$benchmark" == "freqmine" || "$benchmark" == "all" ]]; then
     for i in $(seq $count); do
+        echo running freqmine
         kubectl create -f parsec-benchmarks/part2a/parsec-freqmine.yaml
-        echo run number $i, press key to go on
-        read ok
+        echo run number $i #, press key to go on
+        echo sleeping...
+        sleep 3m # 2 minutes was not enough sometimes
+        #read ok
         ans=$(kubectl logs $(kubectl get pods --selector=job-name=parsec-freqmine --output=jsonpath='{.items[*].metadata.name}'))
         echo "${ans}" | grep user
         #echo "${ans}"
@@ -60,9 +66,11 @@ fi
 
 if [[ "$benchmark" == "ferret" || "$benchmark" == "all" ]]; then
     for i in $(seq $count); do
+        echo running ferret
         kubectl create -f parsec-benchmarks/part2a/parsec-ferret.yaml
-        echo run number $i, press key to go on
-        read ok
+        echo run number $i #, press key to go on
+        sleep 30s
+        #read ok
         ans=$(kubectl logs $(kubectl get pods --selector=job-name=parsec-ferret --output=jsonpath='{.items[*].metadata.name}'))
         echo "${ans}" | grep user
         #echo "${ans}"
@@ -72,6 +80,7 @@ fi
 
 if [[ "$benchmark" == "dedup" || "$benchmark" == "all" ]]; then
     for i in $(seq $count); do
+        echo running dedup
         kubectl create -f parsec-benchmarks/part2a/parsec-dedup.yaml
         echo run number $i #, press key to go on
         echo sleeping...
@@ -86,9 +95,11 @@ fi
 
 if [[ "$benchmark" == "canneal" || "$benchmark" == "all" ]]; then
     for i in $(seq $count); do
+        echo running canneal
         kubectl create -f parsec-benchmarks/part2a/parsec-canneal.yaml
-        echo run number $i, press key to go on
-        read ok
+        echo run number $i #, press key to go on
+        sleep 30s
+        #read ok
         ans=$(kubectl logs $(kubectl get pods --selector=job-name=parsec-canneal --output=jsonpath='{.items[*].metadata.name}'))
         echo "${ans}" | grep user
         #echo "${ans}"
@@ -98,9 +109,12 @@ fi
 
 if [[ "$benchmark" == "blacksholes" || "$benchmark" == "all" ]]; then
     for i in $(seq $count); do
+        echo running blacksholes
         kubectl create -f parsec-benchmarks/part2a/parsec-blacksholes.yaml
-        echo run number $i, press key to go on
-        read ok
+        echo run number $i #, press key to go on
+        echo sleeping...
+        sleep 3m # 2 minutes was not enough sometimes
+        #read ok
         ans=$(kubectl logs $(kubectl get pods --selector=job-name=parsec-blacksholes --output=jsonpath='{.items[*].metadata.name}'))
         echo "${ans}" | grep user
         #echo "${ans}"
@@ -108,4 +122,4 @@ if [[ "$benchmark" == "blacksholes" || "$benchmark" == "all" ]]; then
     done
 fi
 
-#kubectl delete pods --all
+kubectl delete pods --all
