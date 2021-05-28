c23e27ffb0b81bc1a4ed2adf0ca812e856677aaf
PhilMoesch <p.moesch@hotmail.ch>
2021-05-28 02:34:21 +0200

    print to file

--- controllerV3.py
+++ controllerV3.py
@@ -3,6 +3,7 @@ import docker
 import time
 import os
 import sys
+import math
 # sudo docker container stop run_canneal
 # sudo docker container stop run_freqmine
 # sudo docker system prune -a
@@ -50,6 +51,8 @@ pid = sys.argv[1]
 indexContainers2and3 = 0
 indexContainers1 = 4
 
+f = open('ret.txt', 'w')
+
 # let memcached run on cores 0 and 1
 os.system('sudo taskset -a -cp 0-1 ' + pid)
    
@@ -60,8 +63,8 @@ jobCommand = lambda index, t : "/bin/sh -c './bin/parsecmgmt -a run -p " + parse
 jobName = "run_" + parsec_names[indexContainers2and3]
 currentContainer = client.containers.run(parsec_jobs[0], command=jobCommand(indexContainers2and3,2), cpuset_cpus="2-3", detach=True, remove=False, name=jobName)
 runningContainer2and3 = currentContainer
-print(time.time())
-print(jobName + " started")
+print(math.floor(time.time()), file=f)
+print(" " + jobName + " started" + "\n", file=f)
 
 # start canneal job
 # jobCommand = "/bin/sh -c './bin/parsecmgmt -a run -p " + parsec_names[indexJob] + " -i native -n 1'"
@@ -69,8 +72,8 @@ jobName = "run_" + parsec_names[indexContainers1]
 currentContainer = client.containers.run(parsec_jobs[4], command=jobCommand(indexContainers1,1), cpuset_cpus="1", detach=True, remove=False, name=jobName)
 currentContainer.pause()
 runningContainer1 = currentContainer
-print(time.time())
-print(jobName + " started")
+print(math.floor(time.time()), file=f)
+print(" " + jobName + " started" + "\n", file=f)
 #skip = False
 done1 = False
 done2 = False
@@ -85,8 +88,8 @@ while((not done1) or (not done2)):
         myJobName = runningContainer2and3.name
         myContainer = client.containers.get(myJobName)
         if(myContainer.status == "exited"):
-            print(time.time())
-            print(myContainer.name + " done")
+            print(math.floor(time.time()), file=f)
+            print(" " + myContainer.name + " done, ", file=f)
             if (indexContainers2and3 == 3):
                 #all jobs done
                 done1 = True
@@ -100,7 +103,7 @@ while((not done1) or (not done2)):
             stringCpuSet = "2-3"
             newContainer = client.containers.run(parsec_jobs[indexContainers2and3], command=jobCommand(indexContainers2and3,2), cpuset_cpus=stringCpuSet, detach=True, remove=False, name=newJobName)
             runningContainer2and3 = newContainer
-            print(newJobName + " started")
+            print(" " + newJobName + " started" + "\n", file=f)
             #if (indexContainers2and3 == 1):
             #    skip = True 
 
@@ -110,8 +113,8 @@ while((not done1) or (not done2)):
         myJobName = runningContainer1.name
         myContainer = client.containers.get(myJobName)
         if(myContainer.status == "exited"):
-            print(time.time())
-            print(myContainer.name + " done")
+            print(math.floor(time.time()), file=f)
+            print(" " + myContainer.name + " done, ", file=f)
             if (indexContainers1 == 5):
                 #all jobs done
                 done2 = True
@@ -125,7 +128,7 @@ while((not done1) or (not done2)):
             stringCpuSet = "1"
             newContainer = client.containers.run(parsec_jobs[indexContainers1], command=jobCommand(indexContainers1, 1), cpuset_cpus=stringCpuSet, detach=True, remove=False, name=newJobName)
             runningContainer1 = newContainer
-            print(newJobName + " started")
+            print(" " + newJobName + " started" + "\n", file=f)
             #if (indexContainers2and3 == 1):
             #    skip = True 
 
@@ -137,32 +140,32 @@ while((not done1) or (not done2)):
           
         if (cpuNum == 1 and cpu_usages[0] >= 85):
             # increase number of CPUs, pause job running on core 1
-            print(time.time())
+            print(math.floor(time.time()), file=f)
             #print ("status of " + runningContainer1.name + " : " + runningContainer1.status)
             if (indexContainers1 < 5):
-                print("pausing " + runningContainer1.name)
+                print(", pausing " + runningContainer1.name + "\n")
                 runningContainer1.pause()
             #print ("status of " + runningContainer1.name + " : " + runningContainer1.status)
             os.system('sudo taskset -a -cp 0-1 ' + pid)
             cpuNum = 2
         elif(cpuNum == 2 and cpu_usages[0] <= 40):
             # decrease number of CPUs, unpause job running on core 1
-            print(time.time())
+            print(math.floor(time.time()), file=f)
             #print ("status of " + runningContainer1.name + " : " + runningContainer1.status)
             if (indexContainers1 < 5):
-                print("un-pause " + runningContainer1.name)
+                print(", un-pause " + runningContainer1.name + "\n")
                 runningContainer1.unpause()
             #print ("status of " + runningContainer1.name + " : " + runningContainer1.status)
             os.system('sudo taskset -a -cp 0 ' + pid)
             cpuNum = 1
     else:
         if(cpuNum == 1 and cpu_usages[0] >= 85):
-            print(time.time())
+            print(math.floor(time.time()), file=f)
             os.system('sudo taskset -a -cp 0-1 ' + pid)
             runningContainer2and3.update(cpuset_cpus="2-3")
             cpuNum = 2
         elif(cpuNum == 2 and cpu_usages[0] <= 40):
-            print(time.time())
+            print(math.floor(time.time()), file=f)
             os.system('sudo taskset -a -cp 0 ' + pid)
             runningContainer2and3.update(cpuset_cpus="1-3")
             cpuNum = 1
